f92d107f363a1549618dfbc44729ec6775454eef
Hafsa Sarker <73364680+HafsaSarker@users.noreply.github.com>
2023-03-16 02:09:37 -0400

    add query result container

--- caturday/src/App.jsx
+++ caturday/src/App.jsx
@@ -14,6 +14,7 @@ function App() {
         <h1>A fine caturday</h1>
         <h3>Paw-don me, but is this fur real?!</h3>
         <h3>😺</h3>
+        <QueryRes />
         <button>Discover</button>
       </div> 
       <div className="banned-list">
--- caturday/src/Components/QueryRes.jsx
+++ caturday/src/Components/QueryRes.jsx
@@ -1,8 +1,13 @@
 export default function QueryRes(){
     return (
         <div>
+            <h2>Name</h2>
             <div className="attributes">
-                
+                <button>Tonkinese</button>
+                <button>6 - 12 lbs</button>
+                <button>Canada</button>
+                <button>14 - 16 years</button>
+                <img />
             </div>
         </div>
     )
